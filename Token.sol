dff71f10ce6e44ecfc8f3e954aca998ef56ab4c2
Rajnish17060 <rajnishkumarmth12@gmail.com>
2024-07-10 20:23:43 +0530

    Token.sol

---
+++ Token.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor() ERC20("Token", "TK") {
+        _mint(msg.sender, 5000 * 1 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
