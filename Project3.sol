d78d267ff443b97013c2ece3dfca5ded94330bfe
Rajnish17060 <rajnishkumarmth12@gmail.com>
2024-09-05 22:47:07 +0530

    Project3.sol

--- Project3.sol
+++ Project3.sol
@@ -2,17 +2,24 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Token is ERC20 {
-    constructor() ERC20("Token", "TK") {
-        _mint(msg.sender, 5000 * 1 ** decimals());
+contract ERC20Token is ERC20 , Ownable{
+    constructor() ERC20("Token", "TK") Ownable(msg.sender) {
+        _mint(msg.sender, 10 * 10 ** decimals()); 
     }
 
-    function mint(address to, uint256 amount) public {
-        _mint(to, amount);
+    function mint(address recipient, uint256 amount) external onlyOwner {
+        _mint(recipient, amount);
     }
 
-    function burn(uint256 amount) public {
+    function burn(uint256 amount) external {
         _burn(msg.sender, amount);
+    }
+
+    function customTransfer(address from, address to, uint256 amount) public returns (bool) {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        _transfer(from, to, amount);
+        return true;
     }
 }
